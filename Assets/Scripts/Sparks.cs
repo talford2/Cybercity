54e1268a41ceb89dc79b7bfc71dd2d14a62866f5
Steven Alford <steven.alford@collinsonlatitude.com>
2015-12-17 00:45:35 +0800

    Create laser hit effect.

---
+++ Assets/Scripts/Sparks.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Sparks : MonoBehaviour
+{
+    public float Lifetime;
+    public Light SparksLight;
+    public float SparksLightIntensity;
+    public AnimationCurve LightIntensityCurve;
+
+    private float cooldown;
+
+	void Start ()
+	{
+	    cooldown = Lifetime;
+	}
+	
+	void Update () {
+	    if (cooldown >= 0f)
+	    {
+	        cooldown -= Time.deltaTime;
+	        var cooldownFraction = Mathf.Clamp01(cooldown/Lifetime);
+            SparksLight.intensity = SparksLightIntensity * LightIntensityCurve.Evaluate(1f-cooldownFraction);
+	        if (cooldown < 0f)
+	        {
+	            Destroy(gameObject);
+	        }
+	    }
+	}
+}

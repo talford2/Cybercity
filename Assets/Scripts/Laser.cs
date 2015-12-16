33ef3ad4bb3ace06cb7e502bf62b260f42960c75
Steven Alford <steven.alford@collinsonlatitude.com>
2015-12-16 22:09:13 +0800

    FPS modifications.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float MissileLength = 6f;
+    public float MissileSpeed = 150f;
+    public float MissileDamage = 5f;
+
+    public GameObject HitEffectPrefab;
+
+    public GameObject HitDecalPrefab;
+
+    private bool _isLive;
+    private LineRenderer _lineRenderer;
+    private Light _light;
+    private bool _hasHit;
+
+    private GameObject _owner;
+    private Vector3 _shootFrom;
+    private Vector3 _initVelocity;
+    private float _initSpeed;
+    private Vector3 _hitPosition;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _light = GetComponentInChildren<Light>();
+        Stop();
+    }
+
+    public void SetOwner(GameObject owner)
+    {
+        _owner = owner;
+    }
+
+    public void Update()
+    {
+        if (_isLive)
+        {
+            var displacement = (_initSpeed + MissileSpeed)*Time.deltaTime;
+            if (!_hasHit)
+            {
+                var missileRay = new Ray(transform.position, transform.forward);
+                RaycastHit missileHit;
+                if (Physics.Raycast(missileRay, out missileHit, displacement))
+                {
+                    Debug.Log("HIT: " + missileHit.collider.name);
+                    if (missileHit.collider.gameObject != _owner)
+                    {
+                        _hasHit = true;
+                        _hitPosition = missileHit.point;
+
+                        //killable.SendMessageUpwards("Hit", new HitEffectParameters { Position = missileHit.point, Normal = missileHit.normal });
+
+                        // TODO: Should pull this effect from a pool or something...
+                        if (HitEffectPrefab != null)
+                        {
+                            var hitEffectInstance = Instantiate(HitEffectPrefab);
+                            hitEffectInstance.transform.position = missileHit.point;
+                            hitEffectInstance.transform.forward = missileHit.normal;
+                        }
+
+                        if (HitDecalPrefab != null)
+                        {
+                            var hitDecal = Instantiate(HitDecalPrefab);
+                            hitDecal.transform.position = missileHit.point;
+                            hitDecal.transform.SetParent(missileHit.collider.gameObject.transform);
+                            hitDecal.transform.forward = missileHit.normal;
+                        }
+                    }
+                }
+            }
+
+            transform.position += transform.forward*displacement;
+        }
+    }
+
+    public void LateUpdate()
+    {
+        if (_isLive)
+        {
+            UpdateLineRenderer();
+        }
+    }
+
+    public void UpdateLineRenderer()
+    {
+        var headPosition = transform.position;
+        var tailPosition = transform.position - transform.forward*MissileLength;
+
+        var tailDotProd = Vector3.Dot(tailPosition - _shootFrom, transform.forward);
+        var headHitDotProd = Vector3.Dot(headPosition - _hitPosition, transform.forward);
+
+        if (_hasHit && headHitDotProd > 0f)
+        {
+            headPosition = _hitPosition;
+            var tailHitDotProd = Vector3.Dot(tailPosition - _hitPosition, transform.forward);
+            if (tailHitDotProd > 0f)
+            {
+                Stop();
+                return;
+            }
+        }
+
+        if (tailDotProd < 0f)
+        {
+            tailPosition = _shootFrom;
+        }
+
+        _lineRenderer.SetPosition(0, tailPosition);
+        _lineRenderer.SetPosition(1, headPosition);
+    }
+
+    public void Stop()
+    {
+        _isLive = false;
+        _lineRenderer.enabled = false;
+        _light.enabled = false;
+        _hasHit = false;
+    }
+
+    public void Shoot(Vector3 shootFrom, Vector3 direction, Vector3 initVelocity)
+    {
+        _isLive = true;
+        _lineRenderer.enabled = true;
+        _light.enabled = true;
+        _shootFrom = shootFrom;
+        _initVelocity = initVelocity;
+        _initSpeed = _initVelocity.magnitude;
+        _hasHit = false;
+        transform.position += initVelocity*Time.deltaTime;
+        transform.forward = direction;
+        UpdateLineRenderer();
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    #region Public Properties
+
+    public Transform EquippedWeapon;
+    public float ShootTime;
+    public AnimationCurve ShootCurve;
+    public float RestoreTime;
+    public AnimationCurve RestoreCurve;
+
+    [Header("Muzzle Effect")]
+    public float FlashTime;
+    public Light FlashLight;
+    public MeshRenderer MuzzleRenderer;
+
+    [Header("Missile")]
+    public GameObject MissilePrefab;
+
+    #endregion
+
+    #region Private Variables
+
+    private bool isShooting;
+    private float shootCooldown;
+    private bool isRestoring;
+    private float restoreCooldown;
+
+    private Vector3 initialOffset;
+    private Quaternion initialRotation;
+
+    private Vector3 shootOffset;
+    private Quaternion shootRotation;
+
+    private int missilePoolCount = 20;
+    private List<GameObject> missileInstances;
+    private int curMissileIndex;
+
+    private float omega;
+
+    #endregion
+
+    private void Awake()
+    {
+        initialOffset = EquippedWeapon.localPosition;
+        initialRotation = EquippedWeapon.localRotation;
+
+        shootOffset = new Vector3(0f, 0f, -0.25f);
+        shootRotation = Quaternion.Euler(0f, 0f, -10f);
+
+        FlashLight.enabled = false;
+        MuzzleRenderer.enabled = false;
+
+        missileInstances = new List<GameObject>();
+        for (var i = 0; i < missilePoolCount; i++)
+        {
+            missileInstances.Add((GameObject)Instantiate(MissilePrefab, MuzzleRenderer.transform.position, MuzzleRenderer.transform.rotation));
+        }
+        curMissileIndex = 0;
+        omega = 0f;
+    }
+
+    private void ShootMissile()
+    {
+        var missileInstance = missileInstances[curMissileIndex];
+        missileInstance.transform.position = MuzzleRenderer.transform.position;
+        missileInstance.transform.forward = MuzzleRenderer.transform.forward;
+        var laser = missileInstance.GetComponent<Laser>();
+        laser.SetOwner(gameObject);
+        laser.Shoot(MuzzleRenderer.transform.position, MuzzleRenderer.transform.forward, Vector3.zero);
+        curMissileIndex++;
+        if (curMissileIndex >= missilePoolCount)
+            curMissileIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (!isShooting && !isRestoring)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                isShooting = true;
+                shootCooldown = ShootTime;
+                FlashLight.enabled = true;
+                EquippedWeapon.transform.localRotation = initialRotation;
+                MuzzleRenderer.enabled = true;
+                MuzzleRenderer.transform.localRotation = Quaternion.AngleAxis(Random.Range(-180f, 180f), Vector3.forward);
+                FlashLight.intensity = 1f;
+
+                ShootMissile();
+            }
+        }
+
+        if (isShooting)
+        {
+            if (shootCooldown >= 0f)
+            {
+                shootCooldown -= Time.deltaTime;
+
+                if (shootCooldown < (ShootTime - FlashTime))
+                {
+                    FlashLight.enabled = false;
+                    MuzzleRenderer.enabled = false;
+                }
+
+                if (shootCooldown < 0f)
+                {
+                    isShooting = false;
+                    isRestoring = true;
+                    restoreCooldown = RestoreTime;
+                }
+            }
+        }
+
+        if (isRestoring)
+        {
+            if (restoreCooldown >= 0f)
+            {
+                restoreCooldown -= Time.deltaTime;
+                if (restoreCooldown < 0f)
+                {
+                    isRestoring = false;
+                    EquippedWeapon.localPosition = initialOffset;
+                    EquippedWeapon.localRotation = initialRotation;
+                }
+            }
+        }
+
+        if (isShooting)
+        {
+            var shootFraction = ShootCurve.Evaluate(1f - Mathf.Clamp01(shootCooldown/ShootTime));
+            EquippedWeapon.localPosition = Vector3.Lerp(initialOffset, initialOffset + shootOffset, shootFraction);
+            EquippedWeapon.localRotation = Quaternion.Lerp(initialRotation, shootRotation*initialRotation, shootFraction);
+            FlashLight.intensity = 2f*Mathf.Clamp01(shootCooldown/ShootTime);
+        }
+
+        if (isRestoring)
+        {
+            var restoreFraction = RestoreCurve.Evaluate(1f - Mathf.Clamp01(restoreCooldown/RestoreTime));
+            EquippedWeapon.localPosition = Vector3.Lerp(initialOffset + shootOffset, GetIdleOffset(), restoreFraction);
+            EquippedWeapon.localRotation = Quaternion.Lerp(shootRotation * initialRotation, GetIdleRotation(), restoreFraction);
+        }
+
+        omega = (omega + 90f*Time.deltaTime)%360f;
+        if (!isShooting && !isRestoring)
+        {
+            EquippedWeapon.localPosition = GetIdleOffset();
+            EquippedWeapon.localRotation = GetIdleRotation();
+        }
+    }
+
+    private Vector3 GetIdleOffset()
+    {
+        return initialOffset + new Vector3(0f, 0.01f * Mathf.Sin(omega * Mathf.Deg2Rad) - 0.005f, 0f);
+    }
+
+    private Quaternion GetIdleRotation()
+    {
+        return initialRotation*Quaternion.AngleAxis(5f*Mathf.Sin(omega*Mathf.Deg2Rad), Vector3.up);
+    }
+
+    /*
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(20f, 20f, 100f, 30f), string.Format("SHOOT: {0:f2}", Mathf.Clamp01(shootCooldown / ShootTime)));
+        GUI.Label(new Rect(20f, 50f, 100f, 30f), string.Format("RESTORE: {0:f2}", Mathf.Clamp01(restoreCooldown / RestoreTime)));
+    }
+    */
+}
